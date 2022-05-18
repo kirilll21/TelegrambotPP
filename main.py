cf7d6daa9c72002f2a5173e12a8b5a558a4ea145
kirill <kirill2311@yandex.ru>
2022-05-19 00:17:00 +0500

    reform file

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-from commands import*
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+from commands import *
 from config import TOKEN
 
 
-updater = Updater(TOKEN)
+def check_message(update: Update, _: CallbackContext) -> None:
+    text = update.message.text
+    print('Пользователь написал', text)
+    try:
+        update.message.reply_text(weather(text))
+    except Exception as e:
+        print(e)
+        update.message.reply_text("Я вас не понял, если вам нужна помощь введите команду /help")
 
-updater.dispatcher.add_handler(CommandHandler())
-updater.dispatcher.add_handler(CommandHandler())
-updater.dispatcher.add_handler(CommandHandler())
-updater.dispatcher.add_handler(CommandHandler())
-updater.dispatcher.add_handler(CommandHandler())
 
-print('бот запушен')
+def main() -> None:
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
 
-updater.start_polling()
-updater.idle()
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, check_message))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
